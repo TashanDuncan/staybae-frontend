88e8fa96e72bec891e1ce61130d9d3203fb5bb14
Tay Duncan <Tay.Duncan@autotrader.co.uk>
2023-08-18 17:06:52 +0100

    improve nearby Places

--- src/components/places/Places.tsx
+++ src/components/places/Places.tsx
@@ -1,26 +1,30 @@
 import Axios from "src/api/Axios";
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { zhCN } from "date-fns/locale";
+import Rating from "../rating/Rating";
 
-interface Places {
-  name: string
+interface Place {
+  place_id: string;
+  name: string;
+  rating: number
+  // Add more properties as needed
 }
 
 const NearbyPlaces = () => {
-  const [data, setData] = useState<Places[]>([]);
+  const [data, setData] = useState<Place[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
   const { id } = useParams();
 
-
   useEffect(() => {
     Axios.get(`/properties/${id}/places`)
-      .then(response => {
+      .then((response) => {
         setData(response.data.results);
-        console.log(response.data.results)
+        console.log(response.data.results);
         setLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         setError(err);
         setLoading(false);
       });
@@ -36,17 +40,32 @@ const NearbyPlaces = () => {
 
   return (
     <div className="px-4 py-6">
-      <h1 className="text-xl md:text-2xl font-semibold mb-4">20 Nearby Restaurants</h1>
-      <div className="grid grid-cols-2 gap-4">
+      <h1 className="text-xl md:text-2xl font-semibold mb-4">
+        20 Nearby Restaurants
+      </h1>
+      <div className="grid grid-cols-4 gap-4">
         {data.map((place, index) => (
-          <div key={index} className="mb-2">{place.name}</div>
+          <div>
+          <a
+            href={`https://www.google.com/maps/place/?q=place_id:${place.place_id}`}
+            target="_blank"
+            key={index}
+            className="mb-2 text-orange-500 hover:text-blue-700  visited:text-orange-800"
+          >
+            {place.name}
+
+          </a>
+          <Rating
+                id={place.name}
+                ratingScore={place.rating}
+                doSingle={true}
+                propertyRating={place.rating}
+              />
+          </div>
         ))}
       </div>
     </div>
-
   );
 };
 
 export default NearbyPlaces;
-
-
